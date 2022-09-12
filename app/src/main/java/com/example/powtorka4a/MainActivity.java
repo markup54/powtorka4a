2551ff916f3f34f7cb4613d8fbf778467d2d6bce
markup54 <marzenakupka@tlen.pl>
2022-09-12 12:43:17 +0200

    Zadanie z egzaminu

--- app/src/main/java/com/example/powtorka4a/MainActivity.java
+++ app/src/main/java/com/example/powtorka4a/MainActivity.java
@@ -6,24 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText loginEditText;
+TextView komunikatTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginEditText = findViewById(R.id.editTextTextPersonName);
+
+    komunikatTextView = findViewById(R.id.komunikat);
+    komunikatTextView.setText("Autor:00000000000");
     }
 
 
-
-    public void przepisz(View view) {
-        String wpisanyLogin = loginEditText.getText().toString();
-        Toast.makeText(this,wpisanyLogin,Toast.LENGTH_SHORT ).show();
+    public void sprawdz(View view) {
+        EditText mailEditText = findViewById(R.id.editTextTextEmailAddress2);
+        String email = mailEditText.getText().toString();
+        if(!email.contains("@")){
+            komunikatTextView.setText("Nieprawidłowy adres email");
+        }
+        else{
+            EditText hasloEditText1 = findViewById(R.id.editTextTextPassword);
+            EditText hasloEditText2 = findViewById(R.id.editTextTextPassword2);
+            String haslo1 = hasloEditText1.getText().toString();
+            String haslo2 = hasloEditText2.getText().toString();
+            if(!haslo1.equals(haslo2)){
+                komunikatTextView.setText("Hasła się różnią");
+            }
+            else{
+                komunikatTextView.setText("Witaj "+email);
+            }
+        }
 
     }
 }
